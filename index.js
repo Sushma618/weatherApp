243d5690cf073f8d415dbf2e9e003aaa1f38533a
THOKALA SAI SUSHMA <116937279+Sushma618@users.noreply.github.com>
2025-01-06 22:51:03 +0530

    updated

---
+++ index.js
@@ -0,0 +1,5 @@
+const weatherform = document.querySelector(".weatherform");
+const cityInput = document.querySelector(".cityInput");
+const card= document.querySelector(".card");
+const apikey = "1ee42f9f5ef42befcdfe3244bab9def0";
+weatherform.addEventListener("submit")
